5c7933ae867eb25206b0d042d4c05f5b504cc773
Amit Samanta <developer.amitsamanta@gmail.com>
2020-04-06 12:19:44 +0530

    add the component counter

---
+++ src/components/Counter.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+
+    render() {
+        return (
+            <div>
+                <h2>Counter</h2>
+            </div>
+        );
+    }
+}
+
+export default Counter;
